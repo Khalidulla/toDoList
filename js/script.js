51d7ea71a461360c2a480b4a2540c7256a917d7f
Khalidulla <daulethalidulla03@gmail.com>
2024-02-10 04:36:32 +0600

    new

--- js/script.js
+++ js/script.js
@@ -4,18 +4,33 @@ const taskInput = document.querySelector("#taskInput");
 const tasksList = document.querySelector("#tasksList");
 const emptyList = document.querySelector("#emptyList");
 
+let tasks = []; 
+
 form.addEventListener("submit", addTask);
 tasksList.addEventListener("click", deleteTask);
 tasksList.addEventListener("click", doneTask);
+
 
 function addTask(event) {
   //предотвращает перезагрузку страницы
   event.preventDefault();
   //достаем текст задачи из поля ввода
   const taskText = taskInput.value;
+
+  const newTask = {
+    id: Date.now(),
+    text: taskText,
+    done: false,
+  }
+  //добавляем задачу в массив с задачами
+  tasks.push(newTask);
+  console.log(tasks);
+//формируем CSS класс
+  const cssClass = newTask.done ? 'task-title task-title--done'  :  'task-title';
+
   //формируем разметку для новой задачи
-  const taskHTML = `<li class="list-group-item d-flex justify-content-between task-item">
-                          <span class="task-title">${taskText}</span>
+  const taskHTML = `<li id = "${newTask.id}" class="list-group-item d-flex justify-content-between task-item">
+                          <span class="${cssClass}">${newTask.text}</span>
                       <div class="task-item__buttons">
                     <button type="button" data-action="done" class="btn-action">
                      <img src="./img/tick.svg" alt="Done" width="18" height="18">
@@ -41,9 +56,14 @@ function deleteTask(event) {
   //Проводим если клик был НЕ по кнопке "удалить задачу"
   if (event.target.dataset.action !== "delete") return;
 
-  //проверяем что клик был по кнопке "удалить задачу"
-    const parentNode = event.target.closest(".list-group-item");
-    parentNode.remove();
+  const parentNode = event.target.closest(".list-group-item");
+  
+  //Определение ID задачи
+  const  id = parentNode.id;
+  console.log(id)
+  
+  //удаляем задачу из разметки 
+  parentNode.remove();
   //Проверка
   if (tasksList.children.length === 1) {
     emptyList.classList.remove("none");
@@ -52,10 +72,12 @@ function deleteTask(event) {
 
 function doneTask(event) {
   //проверка что клик был НЕ по кнопке "задача выполнена"
-  if (event.target.dataset.action === "done") return;
+  if (event.target.dataset.action === "done") {
 
   //проверка что клик был по кнопке "задача выполнена"
     const parentNode = event.target.closest(".list-group-item");
     const taskTitle = parentNode.querySelector(".task-title");
     taskTitle.classList.toggle("task-title--done");
   }
+}
+
