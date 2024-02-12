9af106feb0abcc7f9c48672e825f0256dad88800
Khalidulla <daulethalidulla03@gmail.com>
2024-02-12 22:28:25 +0600

    new

--- js/script.js
+++ js/script.js
@@ -4,12 +4,11 @@ const taskInput = document.querySelector("#taskInput");
 const tasksList = document.querySelector("#tasksList");
 const emptyList = document.querySelector("#emptyList");
 
-let tasks = []; 
+let tasks = [];
 
 form.addEventListener("submit", addTask);
 tasksList.addEventListener("click", deleteTask);
 tasksList.addEventListener("click", doneTask);
-
 
 function addTask(event) {
   //предотвращает перезагрузку страницы
@@ -21,12 +20,12 @@ function addTask(event) {
     id: Date.now(),
     text: taskText,
     done: false,
-  }
+  };
   //добавляем задачу в массив с задачами
   tasks.push(newTask);
-  console.log(tasks);
-//формируем CSS класс
-  const cssClass = newTask.done ? 'task-title task-title--done'  :  'task-title';
+
+  //формируем CSS класс
+  const cssClass = newTask.done ? "task-title task-title--done" : "task-title";
 
   //формируем разметку для новой задачи
   const taskHTML = `<li id = "${newTask.id}" class="list-group-item d-flex justify-content-between task-item">
@@ -57,12 +56,17 @@ function deleteTask(event) {
   if (event.target.dataset.action !== "delete") return;
 
   const parentNode = event.target.closest(".list-group-item");
-  
+
   //Определение ID задачи
-  const  id = parentNode.id;
-  console.log(id)
-  
-  //удаляем задачу из разметки 
+  const id = Number(parentNode.id);
+
+  //Находим индекс задачи в массиве
+  const index = tasks.findIndex((task) => task.id === id);
+
+  //Удаляем задачу из массива
+  tasks.splice(index, 1);
+
+  //удаляем задачу из разметки
   parentNode.remove();
   //Проверка
   if (tasksList.children.length === 1) {
@@ -73,11 +77,20 @@ function deleteTask(event) {
 function doneTask(event) {
   //проверка что клик был НЕ по кнопке "задача выполнена"
   if (event.target.dataset.action === "done") {
-
-  //проверка что клик был по кнопке "задача выполнена"
+    //проверка что клик был по кнопке "задача выполнена"
     const parentNode = event.target.closest(".list-group-item");
+
+    //Определяем ID задачи
+    const id = Number(parentNode.id);
+
+    const task = tasks.find(function(task){
+      if (task.id === id){
+        return true
+      }
+    })
+    task.done = !task.done;
+
     const taskTitle = parentNode.querySelector(".task-title");
     taskTitle.classList.toggle("task-title--done");
   }
 }
-
