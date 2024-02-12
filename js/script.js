fcfb28d2916dd6b1e6082666e8be35f8c1e7f8b1
Khalidulla <daulethalidulla03@gmail.com>
2024-02-13 01:30:22 +0600

    neeew

--- js/script.js
+++ js/script.js
@@ -6,6 +6,33 @@ const emptyList = document.querySelector("#emptyList");
 
 let tasks = [];
 
+if (localStorage.getItem('tasks')){
+  tasks = JSON.parse(localStorage.getItem('tasks'));
+  console.log(tasks)
+}
+
+tasks.forEach(function(task){
+   //формируем CSS класс
+   const cssClass = task.done ? "task-title task-title--done" : "task-title";
+
+   //формируем разметку для новой задачи
+   const taskHTML = `<li id = "${task.id}" class="list-group-item d-flex justify-content-between task-item">
+                           <span class="${cssClass}">${task.text}</span>
+                       <div class="task-item__buttons">
+                     <button type="button" data-action="done" class="btn-action">
+                      <img src="./img/tick.svg" alt="Done" width="18" height="18">
+                      </button>
+                     <button type="button" data-action="delete" class="btn-action">
+                    <img src="./img/cross.svg" alt="Done" width="18" height="18">
+                    </button>
+                 </div>
+                 </li>`;
+   //добавляем задачу на страницу
+   tasksList.insertAdjacentHTML("beforeend", taskHTML);
+})
+
+checkEmptyList()
+
 form.addEventListener("submit", addTask);
 tasksList.addEventListener("click", deleteTask);
 tasksList.addEventListener("click", doneTask);
@@ -23,6 +50,9 @@ function addTask(event) {
   };
   //добавляем задачу в массив с задачами
   tasks.push(newTask);
+
+  // Сохраняем список задач в хранилище браузера LocalStorage
+  saveToLocalStorage()
 
   //формируем CSS класс
   const cssClass = newTask.done ? "task-title task-title--done" : "task-title";
@@ -45,10 +75,8 @@ function addTask(event) {
   //очищаем поле ввода и возвращаем на него фокус
   taskInput.value = "";
   taskInput.focus();
-  //если в списке задач более 1 элемента скрываем список дел пуст
-  if (tasksList.children.length > 1) {
-    emptyList.classList.add("none");
-  }
+
+  checkEmptyList()
 }
 
 function deleteTask(event) {
@@ -66,12 +94,12 @@ function deleteTask(event) {
   //Удаляем задачу из массива
   tasks.splice(index, 1);
 
+  saveToLocalStorage()
+
   //удаляем задачу из разметки
   parentNode.remove();
-  //Проверка
-  if (tasksList.children.length === 1) {
-    emptyList.classList.remove("none");
-  }
+
+  checkEmptyList(); 
 }
 
 function doneTask(event) {
@@ -90,7 +118,28 @@ function doneTask(event) {
     })
     task.done = !task.done;
 
+    saveToLocalStorage()
+
     const taskTitle = parentNode.querySelector(".task-title");
     taskTitle.classList.toggle("task-title--done");
   }
+}
+
+function checkEmptyList(){
+  if(tasks.length === 0){
+    const emptyListHTML = `<li id="emptyList" class="list-group-item empty-list">
+    <img src="https://img.freepik.com/premium-vector/green-leaf_642458-2233.jpg" alt="Empty" width="48" class="mt-3">
+    <div class="empty-list__title">Список дел пуст</div>
+  </li>`; 
+  tasksList.insertAdjacentHTML('afterbegin', emptyListHTML);
+  }
+  if(tasks.length > 0){
+    const emptyListEl = document.querySelector('#emptyList');
+    emptyListEl ? emptyListEl.remove() : null;
+  }
+}
+
+function saveToLocalStorage(){
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+
 }
